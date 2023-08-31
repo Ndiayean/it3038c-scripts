2f5add26d07e94ce2201a671e2a7f4bbab1454ea
ndiayean <ndiayean@mail.uc.edu>
2023-08-31 19:29:49 -0400

    Lab 2

--- bash/covid.sh
+++ bash/covid.sh
@@ -5,9 +5,13 @@
 DATA=$(curl https://api.covidtracking.com/v1/us/current.json)
 POSITIVE=$(echo $DATA | jq '.[0].positive')
 NEGATIVE=$(echo $DATA | jq '.[0].negative')
+DEATHS=$(echo $DATA | jq '.[0].death')
 HOSPITILIZATIONS=$(echo $DATA | jq '.[0].hospitalizedCumulative')
 STATES=$(echo $DATA | jq '.[0].states')
+NEGATIVEDECREASE=$(echo $DATA | jq '.[0].negativeIncrease')
+TESTRESULTINCREASE=$(echo $DATA | jq '.[0].totalTestResultsIncrease')
+
 
 TODAY=$(date)
 
-echo "On $TODAY, there were $POSITIVE positive COVID cases ,$NEGATIVE negative tests, and $HOSPITILIZATIONS hospitilazations in $STATES states "
+echo "On $TODAY, there were $POSITIVE positive COVID cases ,$NEGATIVE negative tests, $DEATHS deaths, and  $HOSPITILIZATIONS hospitilazations in $STATES states. There is a $NEGATIVEDECREASE increase in negative cases and a increase of $TESTRESULTINCREASE test results. "
