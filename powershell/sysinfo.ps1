fd38302eee501efbca73d6eb05e97de5ad40817f
ndiayean <ndiayean@mail.uc.edu>
2023-09-13 20:24:53 -0700

    Powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,15 @@
+﻿## This will create a function called getIP that retrieves the IPv4 address for the machine
+function getIP{
+
+    (Get-NetIPAddress).IPv4Address | Select-string "10.*"
+
+    }
+
+##This will write the result of the output for the function getIP on the terminal
+write-host(getIP)
+
+##This will store the output of the function getIP into the variable $IP
+$IP = getIP
+
+##This will output the IP address value of the variable $IP on the terminal
+write-host(" This machine's IP is $IP") 
