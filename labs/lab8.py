5b5e1ef3224148ac39ede37af62f7a1b082c1ed5
ndiayean <ndiayean@mail.uc.edu>
2023-10-28 18:14:45 -0400

    Made changed to Lab 8

--- labs/lab8.py
+++ labs/lab8.py
@@ -2,16 +2,14 @@ import requests, re
 
 from bs4 import BeautifulSoup
 
+
+##using get request to retrieve the nike website
 r = requests.get("https://www.nike.com/t/luka-2-basketball-shoes-vcXFrk/DX8733-006").content
 
+##Used buatifulSoup in order to work with the data
 soup = BeautifulSoup( r , "lxml")
 
-## This will search for specific text within the website
-## The first tag will find all "a" tags, then within those "a" tags it will find all "href" attributes that contains "allinone" in the url
-## The second part of the tag is to query the "href" attributes is using re.complile or  regex complie"
-## its telling findall to the use the following regular expresssion within the () on the "href" attribute
-
-
+## This will search for specific text within the website through by specifying tags and classes
 span = soup.find("h1", {"class":"headline-2"})
 title = span.text
 
@@ -19,5 +17,6 @@ span2 = soup.find("div", {"class":"product-price"})
 price = span2.text
 
 
+##Printed out extracted content
 print("On the Nike website the %s cost %s" % (title, price))
 
