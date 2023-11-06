f76dc74cb79607372ac23094d70dd847c1753dd5
ndiayean <ndiayean@mail.uc.edu>
2023-11-05 21:28:35 -0500

    Made change

--- python/weatherapi.py
+++ python/weatherapi.py
@@ -10,7 +10,8 @@ print('please enter your zip code')
 
 zip = input()
 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=e4e4444dc188871deb2a99d2e56faef8' % zip )
+##Removed Api key 
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=yourapikey' % zip )
 
 data = r.json()
 
